b6acb743078c94676574836f8234a7996bc48d16
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-12-15 19:04:21 -0800

    [C] Editor script for detector
    wraps up the parameters into a foldout, and keeps all the callbacks
    visible

---
+++ Assets/_Scripts/Editor/HPUI/HPUIDiscreetGestureDetectorEditor.cs
@@ -0,0 +1,82 @@
+using UnityEditor;
+using UnityEngine.Events;
+using _Scripts;
+
+[CustomEditor(typeof(HPUIDiscreetGestureDetector))]
+public class HPUIDiscreetGestureDetectorEditor : Editor
+{
+    private bool showAdvanced = false;
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        SerializedProperty iterator = serializedObject.GetIterator();
+        bool enterChildren = true;
+
+        while (iterator.NextVisible(enterChildren))
+        {
+            enterChildren = false;
+
+            if (iterator.name == "m_Script")
+            {
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUILayout.PropertyField(iterator, true);
+                EditorGUI.EndDisabledGroup();
+                continue;
+            }
+
+            if (IsUnityEvent(iterator))
+            {
+                EditorGUILayout.PropertyField(iterator, true);
+            }
+        }
+
+        EditorGUILayout.Space();
+        showAdvanced = EditorGUILayout.Foldout(showAdvanced, "Advanced Tuning Properties", true);
+
+        if (showAdvanced)
+        {
+            EditorGUI.indentLevel++;
+
+            iterator = serializedObject.GetIterator();
+            enterChildren = true;
+
+            while (iterator.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+
+                if (iterator.name == "m_Script")
+                    continue;
+
+                if (!IsUnityEvent(iterator))
+                {
+                    EditorGUILayout.PropertyField(iterator, true);
+                }
+            }
+
+            EditorGUI.indentLevel--;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private bool IsUnityEvent(SerializedProperty property)
+    {
+        return property.propertyType == SerializedPropertyType.Generic &&
+               typeof(UnityEventBase).IsAssignableFrom(
+                   fieldInfoFor(property)?.FieldType
+               );
+    }
+
+    private System.Reflection.FieldInfo fieldInfoFor(SerializedProperty property)
+    {
+        var targetType = target.GetType();
+        return targetType.GetField(
+            property.name,
+            System.Reflection.BindingFlags.Instance |
+            System.Reflection.BindingFlags.NonPublic |
+            System.Reflection.BindingFlags.Public
+        );
+    }
+}
